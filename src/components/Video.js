4327e74ef2644041ccd0867287f4fd1f609f19ff
Marcus Ikegami <hikegami.6@gmail.com>
2022-11-30 23:13:19 -0700

    added lazy loading to iframes

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,17 +5,17 @@ const Video = () => {
         <div id="video" className="flex flex-col items-center py-16">
             <h1 className="font-bold text-lg sm:text-3xl text-darkpurple py-8">Video: </h1>
             <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Solo - Black Focus</h3>
-            <iframe src="https://drive.google.com/file/d/199j6p0JUaJzKifX9rJWuOV4ZlxhtIZbQ/preview" width="480" height="350" allow="autoplay" loading="lazy"></iframe>
+            <iframe src="https://drive.google.com/file/d/199j6p0JUaJzKifX9rJWuOV4ZlxhtIZbQ/preview" width="480" height="350" allow="autoplay" loading="lazy" title=""></iframe>
             <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Solo - Chameleon</h3>
-            <iframe src="https://drive.google.com/file/d/1lTT44q_ZxNiMtnyT0g-GMEJKZdZhsUIV/preview" width="480" height="350" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1lTT44q_ZxNiMtnyT0g-GMEJKZdZhsUIV/preview" width="480" height="350" allow="autoplay" loading="lazy" title=""></iframe>
             <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Shrine @ Urban Lounge</h3>
-            <iframe src="https://drive.google.com/file/d/1utu0HDVax-ep1bXBzTVSWZfUPJnYkSwC/preview" width="480" height="350" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1utu0HDVax-ep1bXBzTVSWZfUPJnYkSwC/preview" width="480" height="350" allow="autoplay" loading="lazy" title=""></iframe>
             <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Studio Session - Trio</h3>
-            <iframe src="https://drive.google.com/file/d/1QNqAV5DQyYMRCLzPJnqpKW7MohLs_9GO/preview" width="480" height="350" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1QNqAV5DQyYMRCLzPJnqpKW7MohLs_9GO/preview" width="480" height="350" allow="autoplay" loading="lazy" title=""></iframe>
             <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Jazz Trio @ Hopkins Brewery</h3>
-            <iframe src="https://drive.google.com/file/d/12ZidGsjDPO-awHDm3RY3DYWnFJzWQZtF/preview" width="480" height="350" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/12ZidGsjDPO-awHDm3RY3DYWnFJzWQZtF/preview" width="480" height="350" allow="autoplay" loading="lazy" title=""></iframe>
             <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Ryan Devlin, Alicia Wrigley, Chase Baird @ Hopkins Brewery</h3>
-            <iframe src="https://drive.google.com/file/d/1wv8k5SSRgeCBCbJoLJbepPMmoGAf2GSX/preview" width="480" height="350" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1wv8k5SSRgeCBCbJoLJbepPMmoGAf2GSX/preview" width="480" height="350" allow="autoplay" loading="lazy" title=""></iframe>
         </div>
     )
 }
