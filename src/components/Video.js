a1165a66126f7ee3ebe584ce68254d6924e4c6be
Marcus Ikegami <mikegami.github@gmail.com>
2023-07-06 12:21:10 -0600

    updated homepage

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,27 +1,92 @@
-
-
 const Video = () => {
-    return (
-        <div id="video" className="flex flex-col items-center py-16 px-2">
-            <h1 className="font-bold text-lg sm:text-3xl text-darkpurple py-4">Video: </h1>
-            <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Chase Baird, Ryan Devlin, Alicia Wrigley @ Hopkins Brewery</h3>
-            <iframe src="https://drive.google.com/file/d/1wv8k5SSRgeCBCbJoLJbepPMmoGAf2GSX/preview" width="100%" height="300" allow="autoplay" loading="lazy" title="Ryan Devlin, Alicia Wrigley, Chase Baird @ Hopkins Brewery"></iframe>
-            <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Gene Perla, Ryan Devlin, Steve Kortyka - Live Session</h3>
-            <iframe src="https://drive.google.com/file/d/1cgW_WxzzgSqxuSWwfKrmh9mG0VrxDj_V/preview" width="100%" height="300" allow="autoplay" loading="lazy" title="Emil Welker, Ben Olmstead - Actual Proof Solo"></iframe>
-            <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-4">Shrine @ Urban Lounge</h3>
-            <iframe src="https://drive.google.com/file/d/1utu0HDVax-ep1bXBzTVSWZfUPJnYkSwC/preview" width="100%" height="300" allow="autoplay" loading="lazy" title="Shrine @ Urban Lounge"></iframe>
-            <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Studio Session - Trio</h3>
-            <iframe src="https://drive.google.com/file/d/1QNqAV5DQyYMRCLzPJnqpKW7MohLs_9GO/preview" width="100%" height="300" allow="autoplay" loading="lazy" title="Studio Session - Trio"></iframe>
-            <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Jazz Trio @ Hopkins Brewery</h3>
-            <iframe src="https://drive.google.com/file/d/12ZidGsjDPO-awHDm3RY3DYWnFJzWQZtF/preview" width="100%" height="300" allow="autoplay" loading="lazy" title="Jazz Trio @ Hopkins Brewery"></iframe>
-            <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Solo - Black Focus</h3>
-            <iframe src="https://drive.google.com/file/d/199j6p0JUaJzKifX9rJWuOV4ZlxhtIZbQ/preview" width="100%" height="300" allow="autoplay" loading="lazy" title="Solo - Black Focus"></iframe>
-            <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Solo - Chameleon</h3>
-            <iframe src="https://drive.google.com/file/d/1lTT44q_ZxNiMtnyT0g-GMEJKZdZhsUIV/preview" width="100%" height="300" allow="autoplay" loading="lazy" title="Solo - Chameleon"></iframe>
-            <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">Emil Welker, Ben Olmstead - Actual Proof Solo</h3>
-            <iframe src="https://drive.google.com/file/d/1d74ISWWjqoGkk6StsN0MRVehywE8wvfv/preview" width="100%" height="300" allow="autoplay" loading="lazy" title="Emil Welker, Ben Olmstead - Actual Proof Solo"></iframe>
-        </div>
-    )
-}
+  return (
+    <div id="video" className="flex flex-col items-center py-16 px-2">
+      <h1 className="font-bold text-lg sm:text-3xl text-darkpurple py-4">
+        Video:{" "}
+      </h1>
+      <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">
+        Chase Baird, Ryan Devlin, Alicia Wrigley @ Hopkins Brewery
+      </h3>
+      <iframe
+        src="https://drive.google.com/file/d/1wv8k5SSRgeCBCbJoLJbepPMmoGAf2GSX/preview"
+        width="100%"
+        height="300"
+        allow="autoplay"
+        loading="lazy"
+        title="Ryan Devlin, Alicia Wrigley, Chase Baird @ Hopkins Brewery"
+      ></iframe>
+      <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">
+        Gene Perla, Ryan Devlin, Steve Kortyka - Live Session
+      </h3>
+      <iframe
+        src="https://drive.google.com/file/d/1cgW_WxzzgSqxuSWwfKrmh9mG0VrxDj_V/preview"
+        width="100%"
+        height="300"
+        allow="autoplay"
+        loading="lazy"
+        title="Emil Welker, Ben Olmstead - Actual Proof Solo"
+      ></iframe>
+      <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-4">
+        Shrine @ Urban Lounge
+      </h3>
+      <iframe
+        src="https://drive.google.com/file/d/1utu0HDVax-ep1bXBzTVSWZfUPJnYkSwC/preview"
+        width="100%"
+        height="300"
+        allow="autoplay"
+        loading="lazy"
+        title="Shrine @ Urban Lounge"
+      ></iframe>
+      <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">
+        Studio Session - Trio
+      </h3>
+      <iframe
+        src="https://drive.google.com/file/d/1QNqAV5DQyYMRCLzPJnqpKW7MohLs_9GO/preview"
+        width="100%"
+        height="300"
+        allow="autoplay"
+        loading="lazy"
+        title="Studio Session - Trio"
+      ></iframe>
+      <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">
+        Jazz Trio @ Hopkins Brewery
+      </h3>
+      <iframe
+        src="https://drive.google.com/file/d/12ZidGsjDPO-awHDm3RY3DYWnFJzWQZtF/preview"
+        width="100%"
+        height="300"
+        allow="autoplay"
+        loading="lazy"
+        title="Jazz Trio @ Hopkins Brewery"
+      ></iframe>
+      <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">
+        Solo - Black Focus
+      </h3>
+      <iframe
+        src="https://drive.google.com/file/d/199j6p0JUaJzKifX9rJWuOV4ZlxhtIZbQ/preview"
+        width="100%"
+        height="300"
+        allow="autoplay"
+        loading="lazy"
+        title="Solo - Black Focus"
+      ></iframe>
+      <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">
+        Solo - Chameleon
+      </h3>
+      {/* <iframe src="https://drive.google.com/file/d/1lTT44q_ZxNiMtnyT0g-GMEJKZdZhsUIV/preview" width="100%" height="300" allow="autoplay" loading="lazy" title="Solo - Chameleon"></iframe> */}
+      <h3 className="font-bold text-md sm:text-xl text-darkpurple pb-4 pt-12">
+        Emil Welker, Ben Olmstead - Actual Proof Solo
+      </h3>
+      <iframe
+        src="https://drive.google.com/file/d/1d74ISWWjqoGkk6StsN0MRVehywE8wvfv/preview"
+        width="100%"
+        height="300"
+        allow="autoplay"
+        loading="lazy"
+        title="Emil Welker, Ben Olmstead - Actual Proof Solo"
+      ></iframe>
+    </div>
+  );
+};
 
 export default Video;
